da29e2835ae207c0cb2a8986b1f9de17e1e596d1
saraliva4 <122287923+saraliva4@users.noreply.github.com>
2026-05-10 03:35:45 +0200

    Finished footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,32 @@ export default function App() {
   return (
     <div className="App container">
       <header>
-        <h1 className="text-center">React Dictionary</h1>
+        <h1 className="text-center m-5">React Dictionary</h1>
       </header>
       <main>
         <Dictionary />
       </main>
-      <footer className="text-center">Coded by Sara Littauer</footer>
+      <footer className="text-center mt-5">
+        <a
+          href="https://github.com/saraliva4/Dictionary-React-Add-On-Project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced code
+        </a>{" "}
+        by{" "}
+        <a href="https://github.com/saraliva4" target="_blank" rel="noreferrer">
+          Sara Litauer
+        </a>{" "}
+        hosted on{" "}
+        <a
+          href="https://dictionary-react2-project.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
